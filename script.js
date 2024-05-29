3e64d899635739cb6c29d093f46bc207b15f92a1
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-29 10:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('input[name="volume"]');
+const playbackSpeed = document.querySelector('input[name="playbackSpeed"]');
+const rewind = document.querySelector('.rewind');
+const forward = document.querySelector('.forward');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+// Play and Pause functionality
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+    const icon = video.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+// Volume control
+volume.addEventListener('input', (e) => {
+    video.volume = e.target.value;
+});
+
+// Playback speed control
+playbackSpeed.addEventListener('input', (e) => {
+    video.playbackRate = e.target.value;
+});
+
+// Rewind 10 seconds
+rewind.addEventListener('click', () => {
+    video.currentTime -= 10;
+});
+
+// Forward 25 seconds
+forward.addEventListener('click', () => {
+    video.currentTime += 25;
+});
+
+// Progress bar update
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+// Scrubbing
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+progress.addEventListener('click', scrub);
